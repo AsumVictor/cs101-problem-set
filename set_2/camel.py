04203b973061663c7e4c6192911bff14ff865e55
AsumVictor <iamasum369@gmail.com>
2024-03-15 10:16:11 +0000

    Added: Camel.py

---
+++ set_2/camel.py
@@ -0,0 +1,19 @@
+def main():
+    camel_input = input('Enter your camel case here:  ')
+    snake_case = toSnakeCase(camel_input)
+    print(snake_case)
+
+
+def toSnakeCase(camel: str):
+    result = ''
+    for i in camel:
+        if i.upper() == i:
+            result += '_'
+            result += i.lower()
+        else:
+            result += i.lower()
+
+    return result
+
+
+main()
